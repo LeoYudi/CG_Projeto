6eb8cc531064dd5c3c24e39ae6edd07705109fd7
Leonardo Yudi <lol_leonardohiguti@outlook.com>
2019-11-11 18:13:24 -0300

    adiciona plano

--- Iluminacao/main.js
+++ Iluminacao/main.js
@@ -12,54 +12,60 @@ var esfera = {
 }
 
 var plano = {
-
+  inicio: [0, 0, 0],
+  fim: [100, 100, 0],
+  pontos: []
 }
 
 function main() {
-  let projEsfera = [[]];
   pontosEsfera();
-  projEsfera = projCabinet(esfera);
-  // console.log(projEsfera);
-  printa(projEsfera);
+  pontosPlano();
+  printa(plano.pontos, 'blue');
+  printa(esfera.pontos, 'magenta');
 }
 
-function printa(pontos) {
+function printa(pontos, cor) {
   for (let i = 0; i < pontos.length; i++) {
-    ctx.fillStyle = 'purple';
+    ctx.fillStyle = cor;
     ctx.fillRect(pontos[i][0] + 500, pontos[i][1] + 200, 1, 1);
   }
 }
 
 function pontosEsfera() {
-  for (let a = -Math.PI / 2; a < Math.PI / 2; a += 0.2) {
-    for (let b = -Math.PI; b < Math.PI; b += 0.2) {
+  for (let a = -Math.PI / 2; a < Math.PI / 2; a += 0.02) {
+    for (let b = -Math.PI; b < Math.PI; b += 0.02) {
       let coord = [];
       coord[0] = esfera.raio * Math.cos(a) * Math.cos(b);
       coord[1] = esfera.raio * Math.cos(a) * Math.sin(b);
       coord[2] = esfera.raio * Math.sin(a);
-      coord[3] = 1;
       esfera.pontos.push(coord);
     }
   }
 }
 
-function multiPontoMatriz(ponto, matriz) {
-  let result = [];
-  for (j = 0; j < matriz[0].length; j++) {
-    result[j] = 0;
-    for (k = 0; k < matriz.length; k++) {
-      result[j] += ponto[k] * matriz[k][j];
-    }
-  }
-  return result;
+function pontosPlano() {
+  for (let i = plano.inicio[0]; i < plano.fim[0]; i += 0.1)
+    for (let j = plano.inicio[1]; j < plano.fim[1]; j += 0.1)
+      plano.pontos.push([i, j, 0]);
 }
 
-function projCabinet(obj) {
-  projecaoPontos = [[]];
-  let i;
-  let matrizProj = [[1, 0, 0, 0], [0, 1, 0, 0], [0.4477 / 2, 0.8941 / 2, 0, 0], [0, 0, 0, 1]];
-  for (i = 0; i < obj.pontos.length; i++) {
-    projecaoPontos.push(multiPontoMatriz(obj.pontos[i], matrizProj));
-  }
-  return projecaoPontos;
-}
+// function multiPontoMatriz(ponto, matriz) {
+//   let result = [];
+//   for (j = 0; j < matriz[0].length; j++) {
+//     result[j] = 0;
+//     for (k = 0; k < matriz.length; k++) {
+//       result[j] += ponto[k] * matriz[k][j];
+//     }
+//   }
+//   return result;
+// }
+
+// function projCabinet(obj) {
+//   projecaoPontos = [[]];
+//   let i;
+//   let matrizProj = [[1, 0, 0, 0], [0, 1, 0, 0], [0.4477 / 2, 0.8941 / 2, 0, 0], [0, 0, 0, 1]];
+//   for (i = 0; i < obj.pontos.length; i++) {
+//     projecaoPontos.push(multiPontoMatriz(obj.pontos[i], matrizProj));
+//   }
+//   return projecaoPontos;
+// }
