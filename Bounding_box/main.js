a3998b7afcf6eca9e8f853ef69be9eb443f0af96
Leonardo Yudi <lol_leonardohiguti@outlook.com>
2019-11-22 20:06:24 -0300

    init arquivos do bounding box

---
+++ Bounding_box/main.js
@@ -0,0 +1,10 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var painting = document.getElementById('paint');
+var paint_style = getComputedStyle(painting);
+canvas.width = parseInt(paint_style.getPropertyValue('width'));
+canvas.height = parseInt(paint_style.getPropertyValue('height'));
+
+function main() {
+
+}
