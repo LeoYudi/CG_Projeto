391b70eca4d30607f5e618e4dfa2158196725ca4
Leonardo Yudi <lol_leonardohiguti@outlook.com>
2019-11-23 16:14:45 -0300

    ta fazendo o bounding box agr

--- Bounding_box/main.js
+++ Bounding_box/main.js
@@ -2,13 +2,17 @@ var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 var painting = document.getElementById('paint');
 var paint_style = getComputedStyle(painting);
-var botao = document.getElementById('botao');
+var ligar = document.getElementById('ligar');
 canvas.width = parseInt(paint_style.getPropertyValue('width'));
 canvas.height = parseInt(paint_style.getPropertyValue('height'));
 
 var poligono = {
   vertices: [],
   lados: []
+}
+
+var bounding_box = {
+  vertices: [],
 }
 
 var pegarVertices;
@@ -23,15 +27,45 @@ function main() {
     ctx.fillRect(ponto.x, ponto.y, 3, 3);
     poligono.vertices.push(ponto);
   }, false);
-  botao.addEventListener('click', () => {
+  ligar.addEventListener('click', () => {
     if (poligono.vertices.length > 2) {
-      ctx.fillStyle = 'red';
+      ctx.fillStyle = 'black';
       for (let i = 1; i < poligono.vertices.length; i++)
         poligono.lados.push(bresenhamLinha(poligono.vertices[i - 1], poligono.vertices[i]));
       poligono.lados.push(bresenhamLinha(poligono.vertices[0], poligono.vertices[poligono.vertices.length - 1]));
+      initBoundingBox();
+      ctx.fillStyle = 'red';
+      for (let i = 1; i < bounding_box.vertices.length; i++)
+        bresenhamLinha(bounding_box.vertices[i - 1], bounding_box.vertices[i]);
+      bresenhamLinha(bounding_box.vertices[0], bounding_box.vertices[bounding_box.vertices.length - 1]);
       canvas.removeEventListener('click', pegarVertices, false);
     }
   }, false);
+}
+
+function initBoundingBox() {
+  let maior = {
+    x: -Infinity,
+    y: -Infinity
+  };
+  let menor = {
+    x: Infinity,
+    y: Infinity
+  }
+  for (let i = 0; i < poligono.vertices.length; i++) {
+    if (maior.x < poligono.vertices[i].x)
+      maior.x = poligono.vertices[i].x;
+    if (menor.x > poligono.vertices[i].x)
+      menor.x = poligono.vertices[i].x;
+    if (maior.y < poligono.vertices[i].y)
+      maior.y = poligono.vertices[i].y;
+    if (menor.y > poligono.vertices[i].y)
+      menor.y = poligono.vertices[i].y;
+  }
+  bounding_box.vertices.push(menor);
+  bounding_box.vertices.push({ x: menor.x, y: maior.y });
+  bounding_box.vertices.push(maior);
+  bounding_box.vertices.push({ x: maior.x, y: menor.y });
 }
 
 function bresenhamLinha(inicio, fim) {
